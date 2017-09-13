3f8e636f27c2f3e6c0c0e362632a9ca0f67628f5
aleksandarmihaylov <aleksandaryanev95@gmail.com>
2017-09-13 09:58:03 +0200

    Creating the first class.

---
+++ AutoRepairShop/Models/Shop.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoRepairShop.Models
+{
+    public class Shop
+    {
+        //private inside the class
+        private string name;
+        private string address;
+        private string zip;
+        private string city;
+        private string phone;
+        private string email;
+
+        //constructor
+        public Shop(string Name, string Adress, string Zip, string City, string Phone, string Email)
+        {
+            name = Name;
+            address = Adress;
+            zip = Zip;
+            city = City;
+            phone = Phone;
+            email = Email;
+        }
+        //in order to protect our data we encapsulate
+        // protect our private data and show this to the rest of the world
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public string Adress
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                address = value;
+            }
+        }
+
+        public string Zip
+        {
+            get
+            {
+                return zip;
+            }
+            set
+            {
+                zip = value;
+            }
+        }
+
+        public string City
+        {
+            get
+            {
+                return city;
+            }
+            set
+            {
+                city = value;
+            }
+        }
+
+        public string Phone
+        {
+            get
+            {
+                return phone;
+            }
+            set
+            {
+                phone = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+    }
+}
--- AutoRepairShop/Program.cs
+++ AutoRepairShop/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutoRepairShop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,9 @@ namespace AutoRepairShop
     {
         static void Main(string[] args)
         {
+            Shop myshop = new Shop("MechFix", "Cambridge ST 100", "90001", "Los Angeles", "+1600330213", "myemail@gmail.com");
+            string nameOfTheShop = myshop.Name;
+            Console.WriteLine(myshop.Phone);
         }
     }
 }
